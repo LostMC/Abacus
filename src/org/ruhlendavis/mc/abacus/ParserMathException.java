d15d2fb8bd7f784a79fad2dafdbfb1a95256ca37
Iain E. Davis <iain@ruhlendavis.org>
2012-11-16 10:43:24 -0600

    Forgot to add the new exception class on last commit.

---
+++ src/org/ruhlendavis/mc/abacus/ParserMathException.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ruhlendavis.mc.abacus;
+
+/**
+ *
+ * @author Feaelin
+ */
+public class ParserMathException extends ArithmeticException
+{
+	ParserMathException()
+	{
+		super();
+	}
+
+	ParserMathException(String message)
+	{
+		super(message);
+	}
+}
