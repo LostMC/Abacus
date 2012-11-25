619ab543cc46b2c66eaae1329b0aa7ff040c5efb
Iain Davis <iain@ruhlendavis.org>
2012-11-25 09:46:21 -0600

    Code Cleanup

--- src/org/ruhlendavis/utility/StringTools.java
+++ src/org/ruhlendavis/utility/StringTools.java
@@ -2,9 +2,9 @@ package org.ruhlendavis.utility;
 
 /**
  * A small collection of number related methods that don't belong anywhere else.
- * 
+ *
  * By design, this class cannot be instantiated nor can it be sub-classed.
- * 
+ *
  * @author Feaelin (Iain E. Davis) <iain@ruhlendavis.org>
  */
 public final class StringTools
@@ -20,7 +20,7 @@ public final class StringTools
 	/**
 	 *  Finds the first character in a String that matches any of the characters in
 	 *  another String, starting with the first character of the string.
-	 * 
+	 *
 	 *  @param string String containing the string to search.
 	 *  @param characters String containing the characters to search for.
 	 *  @return int indicating the location in the search string of a character
@@ -31,7 +31,7 @@ public final class StringTools
 	{
 		return find_first_of(string, characters, 0);
 	}
-	
+
 	/**
 	 *  Finds the first character in a String that matches any of the characters in
 	 *  another String
@@ -51,9 +51,5 @@ public final class StringTools
 			}
 		}
 		return -1;
-	}
-	public static void main(String [] a)
-	{
-		System.out.println(StringTools.find_first_of("", "a", 0));
 	}
 }
